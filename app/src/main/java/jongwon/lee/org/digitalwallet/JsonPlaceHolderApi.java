064936ac802321b07ac824b1ceadae98cc446d53
Joey Lee <jongwon254@gmail.com>
2022-05-29 23:01:51 +0200

    Initial commit

---
+++ app/src/main/java/jongwon/lee/org/digitalwallet/JsonPlaceHolderApi.java
@@ -0,0 +1,19 @@
+package jongwon.lee.org.digitalwallet;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
+public interface JsonPlaceHolderApi {
+
+    @GET("key")
+    Call<List<Key>> getKeys(@Header("Authorization") String authHeader);
+
+    @POST("auth/login")
+    Call<User> createUser(@Body User user);
+}
---
+++ app/src/main/java/jongwon/lee/org/digitalwallet/MainActivity.java
@@ -0,0 +1,65 @@
+package jongwon.lee.org.digitalwallet;
+
+import androidx.appcompat.app.AppCompatActivity;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textViewResult = findViewById(R.id.text_view_result);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://10.0.2.2:8080/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+
+        String authToken = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0MkB0ZXN0LmNvbSIsImlhdCI6MTY1Mzg0MDg0MywiZXhwIjoxNjU0NDQ1NjQzfQ.vwQqXaRPDogHf1DpTtyNoABLIO6msCl6iyv_lNnBHdg";
+
+        Call<List<Key>> call = jsonPlaceHolderApi.getKeys("Bearer " + authToken);
+
+        call.enqueue(new Callback<List<Key>>() {
+            @Override
+            public void onResponse(Call<List<Key>> call, Response<List<Key>> response) {
+                if(!response.isSuccessful()) {
+                    textViewResult.setText("Code: " + response.code());
+                    return;
+                }
+
+                List<Key> keys = response.body();
+
+                for(Key key : keys) {
+                    String content = "";
+                    content += "ID: " + key.getId() + "\n";
+                    content += "First Name: " + key.getFirstName() + "\n";
+                    content += "Last Name: " + key.getLastName() + "\n";
+                    content += "Car Brand: " + key.getCarBrand() + "\n";
+                    content += "Car Model: " + key.getCarModel() + "\n\n";
+
+                    textViewResult.append(content);
+                 }
+            }
+
+            @Override
+            public void onFailure(Call<List<Key>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+}
